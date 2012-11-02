2a2ebb21fcf08488059c8fde8455a795e766e811
naelstrof <naelstrof@gmail.com>
2012-11-02 11:32:59 +0000

    base started

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+
+    <head>
+        <!-- Yay metadata! -->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
+        <meta name="Description" content="Professionally developing mobile applications and websites for clients.">
+
+        <title>Nomadic Developers</title>
+        <link rel="shortcut icon"   href="favicon.png">
+        <link rel="stylesheet"      href="main.css">
+    </head>
+
+    <link rel="stylesheet" type="text/css" href="data:text/css,">
+
+    <!-- Content -->
+    <body>
+        <div id="column">
+            <div id="header" class="box">
+                Images of company name + navigation buttons
+            </div>
+            <div id="content" class="box">
+                Description of what we do with examples of our work.
+            </div>
+        </div>
+    </body>
+
+</html>
